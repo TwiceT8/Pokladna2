37c9d0facedf1880f4a639fc05fd76a9a5febe5c
Jaromír Kleveta <Kleveta.Jaromir@stud.vassboskovice.cz>
2020-10-13 10:41:37 +0200

    vytvoreni Irepos

--- Pokladna/Form1.cs
+++ Pokladna/Form1.cs
@@ -12,9 +12,15 @@ namespace Pokladna
 {
     public partial class Form1 : Form
     {
+        List<PoklZaznam> pokladna;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pokladna = 
         }
     }
 }
